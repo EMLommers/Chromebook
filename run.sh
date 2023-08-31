9afec3f7749725e9ddd0bceddc8d386f36f505e3
Accumulator <79648274+EMLommers@users.noreply.github.com>
2023-08-31 19:28:43 +0200

    bash file automatic

---
+++ run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+ 
+ cp *0.bin 235.bin
+ cbfstool 235.bin extract -n vbgfx.bin -f vbgfx.bin -m arm64
+ cbfstool 235.bin extract -n font.bin -f font.bin -m arm64
+  cbfstool 235.bin extract -n locale_en.bin -f  locale.bin -m arm64
+# this file in same directory
+python3 ./bitmap_mod.py
+cbfstool 235.bin remove -n vbgfx.bin
+cbfstool 235.bin remove -n font.bin
+cbfstool 235.bin remove -n locale_en.bin
+cbfstool 235.bin add -n vbgfx.bin -f vbgfx.bin -c lzma -t raw
+cbfstool 235.bin add -n locale_en.bin -f locale_en.bin -c lzma -t raw
+cbfstool 235.bin add -n font.bin -f font.bin -c lzma -t raw
+flashrom -p host -w 235.bin
+exit
+
